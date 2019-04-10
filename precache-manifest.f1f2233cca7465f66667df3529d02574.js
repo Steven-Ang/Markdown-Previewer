22b9b88cd38928c807ef254442f6fcfb746de80b
Steven <spikegreen2@gmail.com>
2019-04-10 21:24:20 +0800

    Updates

---
+++ precache-manifest.f1f2233cca7465f66667df3529d02574.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "cb9e916f8f13af65eff1",
+    "url": "/static/css/main.b75580cd.chunk.css"
+  },
+  {
+    "revision": "cb9e916f8f13af65eff1",
+    "url": "/static/js/main.a8b33af7.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "d369c8e02aa24c4a3dc1",
+    "url": "/static/js/2.0b52dbfd.chunk.js"
+  },
+  {
+    "revision": "afc9f04cdee1f6b1af976f7ef125849e",
+    "url": "/index.html"
+  }
+];
